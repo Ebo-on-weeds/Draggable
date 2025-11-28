a357f4e2276f60edc9faf2b07bcf7c51046c94d7
Ebo-on-weeds <ebarheema90@gmail.com>
2025-11-29 00:01:47 +0300

    chore(setup): configure commitlint and import alias

---
+++ commitlint.config.cjs
@@ -0,0 +1,13 @@
+
+
+
+module.exports = {extends: ['@commitlint/config-conventional'],rules:{
+    'scope-empty':[2,'never'],
+    'scope-enum': [2, 'always', ['setup', 'dashboard', 'profile', 'login','editor','api']],
+    "type-enum":[
+        2,'always',[
+            'feat','fix','docs','chore','style','test','refactor'
+        ]
+    ],
+
+}};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve:{
+    alias:{
+      "@":path.resolve(__dirname,"./src"),
+      "@app":path.resolve(__dirname,"./src/app"),
+      "@components":path.resolve(__dirname,"./components"),
+      "@config":path.resolve(__dirname,"./config"),
+      "@hooks":path.resolve(__dirname,"./hooks"),
+      "@pages":path.resolve(__dirname,"./pages"),
+      "@stores":path.resolve(__dirname,"./stores"),
+      "@testing":path.resolve(__dirname,"./testing"),
+    }
+  },
   plugins: [react()],
 })
